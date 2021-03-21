1c4e1d71cc7d6338e0f313d016864f51b5e16dbe
Anastasiia Romanenko <druzhkoshowewed@gmail.com>
2021-03-21 11:39:13 +0300

    first one

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect
+
+from data.jobs import Jobs
+from data.users import User
+from data import db_session
+
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
+
+
+if __name__ == '__main__':
+    main()
+
+
